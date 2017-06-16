ffc7efcca58c6e7b465260a95927716eee1b9666
root <root@betagom-1.ad.ea.com>
2017-06-16 16:59:57 +0800

    [script] update template, and add sync docker images script

--- script/template/python_tools_template.py
+++ script/template/python_tools_template.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
 #_*_coding:utf8_*_
+
 from __future__ import with_statement
 from optparse import OptionParser
 from os import system as sh
@@ -7,6 +8,7 @@ from time import strftime
 import base64, random
 import sys, os, time, commands, re, string
 import getpass,subprocess
+import commands;
 
 
 cmds = ["help"]
@@ -21,7 +23,32 @@ def usage(option, opt, value, parser):
     for item in cmds:
         cmd_help = eval("%s.__doc__" % item)
         print cmd_help.split("\n")[0]
-    sys.exit(0)
+
+def get_lazy_cmd(cmd_name):
+    if cmd_name in cmds: return cmd_name
+    lazy_cmds = [cmd for cmd in cmds if cmd.startswith(cmd_name)]
+    if lazy_cmds == []:
+        print "匹配不到命令:%s" % cmd_name
+        return None
+    if len(lazy_cmds) > 1:
+        print "命令不能唯一匹配：", lazy_cmds
+        return None
+    return lazy_cmds[0]
+
+def help(argv):
+    """  help\t\thelp cmd显示命令的详细帮助
+
+  ./syncImagess help db (显示restart的详细帮助信息)
+"""
+    if argv == []: 
+        cmd_list = cmds[:]
+    elif argv[0] in cmds:
+        cmd_list = [cmds[cmds.index(argv[0])]]
+    else:
+        cmd_list = ["help"]
+    for cmd in cmd_list:
+        cmd_help = eval("%s.__doc__" % cmd)
+        print cmd_help
 
 def run():
     try:     
